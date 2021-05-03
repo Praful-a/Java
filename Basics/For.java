5a2771f0ee9f6dcb70873c6f1cfda53f3ce5d380
Praful <prafuldubey27@gmail.com>
2021-05-03 23:40:23 +0530

    removing class files.

--- Basics/For.java
+++ Basics/For.java
@@ -1,5 +1,5 @@
 // Program to illustrate infinite loop
-public class For
+/*public class For
 {
 	public static void main(String[] args)
 	{
@@ -8,4 +8,21 @@ public class For
 			System.out.println("This is an infinite loop");
 		}
 	}
+}*/
+
+// Program to initializing multiple variables in 
+// initialization block
+public class For
+{
+
+	public static void main(String[] args)
+	{
+		int x = 2;
+		for(long y=0, z = 4; x < 10 && y < 10; x++, y++)
+		{
+			System.out.println(y + " ");
+		}
+		System.out.println(x);
+	}
 }
+

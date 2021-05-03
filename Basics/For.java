cd840e7c032c1c9cff609ae21a342e2d0c3f0b69
Praful <prafuldubey27@gmail.com>
2021-05-03 11:20:18 +0530

    infinite loop

---
+++ Basics/For.java
@@ -0,0 +1,11 @@
+// Program to illustrate infinite loop
+public class For
+{
+	public static void main(String[] args)
+	{
+		for( ; ; )
+		{
+			System.out.println("This is an infinite loop");
+		}
+	}
+}

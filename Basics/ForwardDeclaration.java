d7c230288d5b63b6dfd44a2935ac91ac55da9a4d
Praful <prafuldubey27@gmail.com>
2021-05-04 14:33:26 +0530

    forward declaration.

---
+++ Basics/ForwardDeclaration.java
@@ -0,0 +1,13 @@
+class ForwardDeclaration
+{
+	public static void main(String[] args) {
+		Test1 t1 = new Test1();
+		t1.fun(5);
+	}
+}
+
+class Test1 {
+	void fun(int x) {
+		System.out.println("fun() called: x = "+ x);
+	}
+}

c2268f4d0fdeaab87d7f3df53dbde690e0d86d10
Praful <prafuldubey27@gmail.com>
2021-05-04 08:41:01 +0530

    if, if_else, ladder if.

--- Basics/IfDemo.java
+++ Basics/IfDemo.java
@@ -1,5 +1,5 @@
 // Program to illustrate if statement
-class IfDemo
+/*class IfDemo
 {
 	public static void main(String args[])
 	{
@@ -10,5 +10,64 @@ class IfDemo
 		// This statement will be executed
 		// as if considers one statement by default
 		System.out.println("I am Not in if.");
+	}
+}*/
+
+// Program to illustrate if-else statement
+/*class IfDemo
+{
+	public static void main(String args[])
+	{
+		int i = 10;
+		if(i < 15)
+			System.out.println("i is smaller than 15.");
+		else
+			System.out.println("i is greater than 15");
+	}
+}*/
+/*
+class IfDemo
+{
+	public static void main(String args[])
+	{
+		int a = -12, b=-11, c = 1;
+		if(a>b)
+		{
+			if(a>c)
+			{
+				System.out.println(a+" is greater.");
+			}
+			else
+			{
+				System.out.println(c+" is greater");
+			}
+		}
+		else
+		{
+			if (b>c)
+			{
+				System.out.println(b+" is greater");
+			}
+			else
+			{
+				System.out.println(c+" is greater");
+			}
+		}
+	}
+}*/
+
+class IfDemo
+{
+	public static void main(String args[])
+	{
+		int i = 20;
+		if(i == 10)
+			System.out.println("i is 10.");
+		else if (i == 15)
+			System.out.println("i is 15");
+		else if (i == 20)
+			System.out.println("i is 20");
+		else
+			System.out.println("i is not present.");
 	}
 }

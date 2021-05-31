ebe2931952bb2c0564d444bf400e0ab5d2d3c417
rock <prafuldubey27@gmail.com>
2021-05-31 17:23:25 +0530

    printing Primitive data types default values without using objects.

---
+++ program/Primitive.java
@@ -0,0 +1,23 @@
+public class Primitive {
+	static boolean val1;
+	static double val2;
+	static float val3;
+	static int val4;
+	static long val5;
+	static String val6;
+	static byte b;
+	static short s;
+	static char ch;
+	public static void main(String[] args) {
+		System.out.println(" Default Values....");
+		System.out.println(" Boolean = " + val1);
+		System.out.println(" Double = " + val2);
+		System.out.println(" Float = " + val3);
+		System.out.println(" Integer = " + val4);
+		System.out.println(" Long = " + val5);
+		System.out.println(" String = " + val6);
+		System.out.println(" byte = " + b);
+		System.out.println(" Short = " + s);
+		System.out.println(" Char = " + ch);
+	}
+}

0756d00a65c2f4b6c13bf3854ee171a118838d5a
rock <prafuldubey27@gmail.com>
2021-06-23 17:51:01 +0530

    Byte DataType

---
+++ datatypes/primitive/ByteEx.java
@@ -0,0 +1,10 @@
+class ByteEx {
+	public static void main(String args[]) {
+		byte n, a;
+		n = 127;
+		// a = 177;
+		System.out.println(n);
+		// throws an error because it cannot store more than 127 bits.
+		// System.out.println(a); 
+	}
+}

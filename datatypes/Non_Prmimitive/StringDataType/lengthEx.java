14682d34c4bd7c2b5e12dc5943215983bb91cf76
rock <prafuldubey27@gmail.com>
2021-06-23 18:17:53 +0530

    length method of string data type

---
+++ datatypes/Non_Prmimitive/StringDataType/lengthEx.java
@@ -0,0 +1,10 @@
+public class lengthEx {
+	static String str;
+	public static void main(String[] args) {
+		String s1 = "Hello";
+		String s2 = "Whatsup";
+		System.out.println(" String length is : " + s1.length());
+		System.out.println(" String length is : " + s2.length());
+		System.out.println(" Default value : " + str);	
+	}
+}

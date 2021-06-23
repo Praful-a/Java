334991f08fef365e5d1767ff68324616f9eaef42
rock <prafuldubey27@gmail.com>
2021-06-23 18:41:04 +0530

    replace method.

---
+++ datatypes/Non_Prmimitive/StringDataType/replaceEx.java
@@ -0,0 +1,16 @@
+/*public class replaceEx {
+	public static void main(String[] args) {
+		String s1 = "hello how are you";
+		String replaceString = s1.replace('h', 't');
+		System.out.println(replaceString);
+	}
+}*/
+
+public class replaceEx {
+	public static void main(String[] args) {
+		String s1 = "Hey, welcome to America";
+		String replaceString = s1.replace("America", "India");
+		System.out.println(replaceString);
+	}
+}
+

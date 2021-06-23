3c4c6097ec4fd68928d7c9ef991ce01dfc9e2575
rock <prafuldubey27@gmail.com>
2021-06-23 17:47:13 +0530

    boolean data type.

---
+++ datatypes/primitive/booleanDataType.java
@@ -0,0 +1,11 @@
+class booleanDataType {
+	static boolean C;
+	public static void main(String args[]) {
+		// Setting the values for boolean data type
+		boolean Java = true;
+		boolean Python = false;
+		System.out.println(Java);
+		System.out.println(Python);
+		System.out.println(C);
+	}
+}

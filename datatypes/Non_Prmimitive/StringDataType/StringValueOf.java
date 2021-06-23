b85d41f1f8c118e121fc93ef2be523a574a4fdf6
rock <prafuldubey27@gmail.com>
2021-06-23 18:38:18 +0530

    valueOf method

---
+++ datatypes/Non_Prmimitive/StringDataType/StringValueOf.java
@@ -0,0 +1,7 @@
+public class StringValueOf {
+	public static void main(String[] args) {
+		int value = 20;
+		String s1 = String.valueOf(value);
+		System.out.println(s1+17);  // concatinating string with number
+	}
+}

d94bc8c72e67701141ab2f572a2220b709165c32
rock <prafuldubey27@gmail.com>
2021-06-23 18:31:38 +0530

    trim method

---
+++ datatypes/Non_Prmimitive/StringDataType/trimEx.java
@@ -0,0 +1,7 @@
+public class trimEx {
+	public static void main(String[] args) {
+		String s1 = "   hello  ";
+		System.out.println(s1 + " how are you");
+		System.out.println(s1.trim() + " how are you");
+	}
+}

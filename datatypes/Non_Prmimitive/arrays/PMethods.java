04bbf72867fe5b6c5c6df74ca39a751294b6fc07
rock <prafuldubey27@gmail.com>
2021-06-24 16:45:33 +0530

    arrays in java

---
+++ datatypes/Non_Prmimitive/arrays/PMethods.java
@@ -0,0 +1,11 @@
+public class PMethods {
+	public static void display(int y[]) {
+		System.out.println(y[0]);
+		System.out.println(y[1]);
+		System.out.println(y[2]);
+	}
+	public static void main(String[] args) {
+		int x[] = {1, 2, 3};
+		display(x);
+	}
+}

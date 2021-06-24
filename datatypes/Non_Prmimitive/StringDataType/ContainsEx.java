9f8ff7e41ab8d0a7455531590f3518c076872c0f
rock <prafuldubey27@gmail.com>
2021-06-24 09:35:40 +0530

    contains method

---
+++ datatypes/Non_Prmimitive/StringDataType/ContainsEx.java
@@ -0,0 +1,8 @@
+class ContainsEx {
+	public static void main(String args[]) {
+		String name = " hello how are you doing";
+		System.out.println(name.contains("how are you"));
+		System.out.println(name.contains("hello"));
+		System.out.println(name.contains("fine"));
+	}
+}

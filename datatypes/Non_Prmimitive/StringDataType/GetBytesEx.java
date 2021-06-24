ede0089451245c0bd7d264036947907ddc0cebb0
rock <prafuldubey27@gmail.com>
2021-06-24 09:46:00 +0530

    getbytes method

---
+++ datatypes/Non_Prmimitive/StringDataType/GetBytesEx.java
@@ -0,0 +1,9 @@
+public class GetBytesEx {
+	public static void main(String[] args) {
+		String s1 = "ABC";
+		byte[] b = s1.getBytes();
+		for(int i = 0; i < b.length; i++) {
+			System.out.println(b[i]);
+		}
+	}
+}

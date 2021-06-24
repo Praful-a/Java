5e91cfe19e32ab60000184d1d2cd9d33750756d3
rock <prafuldubey27@gmail.com>
2021-06-24 09:41:39 +0530

    equals Ignore Case method.

---
+++ datatypes/Non_Prmimitive/StringDataType/EqualsIgnore.java
@@ -0,0 +1,9 @@
+public class EqualsIgnore {
+	public static void main(String[] args) {
+		String s1 = "hello";
+		String s2 = "HELLO";
+		String s3 = "hi";
+		System.out.println(s1.equalsIgnoreCase(s2));
+		System.out.println(s1.equalsIgnoreCase(s3));
+	}
+}

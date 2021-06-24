4b61d9e1fb3761067023e0ccf131bb03f44a42e3
rock <prafuldubey27@gmail.com>
2021-06-24 09:38:02 +0530

    eqals method.

---
+++ datatypes/Non_Prmimitive/StringDataType/EqualsEx.java
@@ -0,0 +1,9 @@
+public class EqualsEx {
+	public static void main(String[] args) {
+		String s1 = "hello";
+		String s2 = "hello";
+		String s3 = "hi";
+		System.out.println(s1.equals(s2));
+		System.out.println(s1.equals(s3));
+	}
+}

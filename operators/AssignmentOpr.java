ad5fc56291074973a3c908eaf9354096f2841baf
rock <prafuldubey27@gmail.com>
2021-06-24 17:18:36 +0530

    equility and relational operator

---
+++ operators/AssignmentOpr.java
@@ -0,0 +1,14 @@
+class AssignmentOpr {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c;
+		System.out.println(c = a);
+		System.out.println(b += a);
+		System.out.println(b -= a);
+		System.out.println(b *= a);
+		System.out.println(b /= a);
+		System.out.println(b %= a);
+		System.out.println(b ^= a);
+	}
+}
---
+++ operators/ComparisonDemo.java
@@ -0,0 +1,16 @@
+class ComparisonDemo {
+	public static void main(String[] args) {
+		int val1 = 1;
+		int val2 = 2;
+		if(val1 == val2)
+			System.out.println("value 1 == value 2");
+		if(val1 != val2) 
+			System.out.println("value 1 != value 2");
+		if(val1 > val2) 
+			System.out.println("value 1 > Value 2");
+		if(val1 < val2) 
+			System.out.println("value 1 < value 2");
+		if(val1 <= val2) 
+			System.out.println("value 1 <= value 2");
+	}
+}

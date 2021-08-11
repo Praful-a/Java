22b3e44aac22fa2258f74cdfb2951588877e3524
Praful-a <prafuldubey27@gmail.com>
2021-08-11 17:37:07 +0530

    static and concrete classes

---
+++ classes&Objects/ClassesTypes/Concrete.java
@@ -0,0 +1,9 @@
+public class Concrete {
+	static int sum(int x, int y) {
+		return x + y;
+	}
+	public static void main(String args[]) {
+		int p = sum(6, 8);
+		System.out.println("Sum: " + p);
+	}
+}
